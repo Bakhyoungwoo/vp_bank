4b95dfd6ef0d17d79d7015907d3d12cfe361603a
박형우 <128560371+Bakhyoungwoo@users.noreply.github.com>
2026-01-21 17:36:56 +0900

    refactor : 불필요한 파일 삭제

--- src/main/java/com/example/vap_back/repository/TransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.vap_back.repository;
-
-import com.example.vap_back.Entity.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-}
