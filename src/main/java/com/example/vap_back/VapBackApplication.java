2068acaaffbadebb621c26ee284d852d69cd00ca
박형우 <128560371+Bakhyoungwoo@users.noreply.github.com>
2026-01-24 16:49:10 +0900

    refactor: fastapi에서 보내는 뉴스에 대해 권한 허용

--- src/main/java/com/example/vap_back/VapBackApplication.java
+++ src/main/java/com/example/vap_back/VapBackApplication.java
@@ -1,11 +1,22 @@
 package com.example.vap_back;
 
+import com.example.vap_back.service.NewsTestService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VapBackApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(VapBackApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner testNewsSave(NewsTestService service) {
+        return args -> {
+            service.saveTestNews();
+        };
     }
 }
