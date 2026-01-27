0e4c030f2382241d308ac14992e6c891117499a9
박형우 <128560371+Bakhyoungwoo@users.noreply.github.com>
2026-01-27 11:05:24 +0900

    bug : Keywords와 시간정보를 읽을 수 없었던 버그

--- src/main/java/com/example/vap_back/dto/NewsCreateRequest.java
+++ src/main/java/com/example/vap_back/dto/NewsCreateRequest.java
@@ -1,9 +1,10 @@
 package com.example.vap_back.dto;
 
 import lombok.Getter;
-
 import java.time.LocalDateTime;
-
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonFormat;
 @Getter
 public class NewsCreateRequest {
 
@@ -12,5 +13,8 @@ public class NewsCreateRequest {
     private String content;
     private String url;
     private String press;
+    @JsonProperty("published_at")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime publishedAt;
+    private List<String> keywords;
 }
