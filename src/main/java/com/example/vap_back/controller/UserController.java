f8f8c8cf6c4d32498eb7ee862a74c2e668e25138
박형우 <128560371+Bakhyoungwoo@users.noreply.github.com>
2026-01-30 15:15:36 +0900

    feat : 자동로그인 기능 삭제

--- src/main/java/com/example/vap_back/controller/UserController.java
+++ src/main/java/com/example/vap_back/controller/UserController.java
@@ -26,24 +26,6 @@ public class UserController {
     public String login(@RequestBody UserRequest request) {
         return userService.login(request.getEmail(), request.getPassword());
     }
-    // 자동 로그인
-    @GetMapping("/me")
-    public ResponseEntity<?> getCurrentUser(Authentication authentication) {
-        if (authentication == null || !authentication.isAuthenticated()) {
-            return ResponseEntity.status(401).body("로그인이 필요합니다.");
-        }
-
-        // 인증된 사용자 정보 가져오기
-        String email = authentication.getName();
-        User user = userService.getUserByEmail(email); // (서비스 이름이 userInterestService일 수도 있음. 확인 필요)
-
-        // 닉네임과 이메일 반환
-        return ResponseEntity.ok(Map.of(
-                "email", user.getEmail(),
-                "nickname", user.getName()
-        ));
-    }
-
     @GetMapping
     public List<User> list() {
         return userService.findAll();
