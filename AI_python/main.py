5b5bd7cdb0b7311e41baffd0304e15e62481b958
BakHyeongwo <toplionby421@gmail.com>
2026-09-15 23:48:14 +0900

    chore: 주석에서 KIS 언급 제거

--- AI_python/main.py
+++ AI_python/main.py
@@ -9,7 +9,7 @@ from contextlib import asynccontextmanager
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from dotenv import load_dotenv
 
-# uvicorn을 어느 작업 디렉터리에서 실행하든 OPENAI_API_KEY/KIS_* 값을
+# uvicorn을 어느 작업 디렉터리에서 실행하든 OPENAI_API_KEY 값을
 # 읽을 수 있도록 AI_python/.env 경로를 명시해서 로드한다.
 load_dotenv(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".env"))
 
